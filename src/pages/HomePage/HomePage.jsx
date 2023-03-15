cd800483c269fc22ba8ee6d5d6bae8504020ffc5
Dinara83 <mavleeva.dinara@gmail.com>
2023-03-15 13:37:07 +0200

    image

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,18 @@
-import css from './home-page.module.css'
+import css from './home-page.module.css';
 
 const HomePage = () => {
   return (
-	<div className={css.phoneBook}>
-		<p className={css.textPhoneBook}>Wellcome to PHONE BOOK</p>
-        <picture>
-          <source
-            srcset="https://static.wixstatic.com/media/bdefd8_f6b7b86bbd354320b0db1b8260028902~mv2.gif"
-          />
-          <img
-            src="https://static.wixstatic.com/media/bdefd8_f6b7b86bbd354320b0db1b8260028902~mv2.gif"
-            className={css.animatePhoneBook}
-            alt="phonebook"
-            width={500}
-            height={300}
-          />
-        </picture>
-  </div>
-  )
+    <div className={css.phoneBook}>
+      <p className={css.textPhoneBook}>Wellcome to PHONEBOOK</p>
+      <img
+        src="https://static.wixstatic.com/media/bdefd8_f6b7b86bbd354320b0db1b8260028902~mv2.gif"
+        className={css.animatePhoneBook}
+        alt="phonebook"
+        width={500}
+        height={300}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
